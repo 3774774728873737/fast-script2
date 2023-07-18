a6be4cf49b4be09f38a9f3767a5b0d6dbb38a917
3774774728873737 <138348862+3774774728873737@users.noreply.github.com>
2023-07-19 04:18:43 +0500

    Update script.py

--- script.py
+++ script.py
@@ -1,124 +1,174 @@
 from fastapi import FastAPI, UploadFile, File, Form
-from fastapi.responses import JSONResponse, FileResponse, Response
-from fastapi.staticfiles import StaticFiles
+from fastapi.responses import JSONResponse, FileResponse
 import cv2
 from fastapi.middleware.cors import CORSMiddleware
+from moviepy.editor import VideoFileClip, clips_array
+from moviepy.editor import *
+import os
+import base64
+import uvicorn
 from typing import List
 import subprocess
-import os
-import uuid
+import time
+import random
+import string
+global audioname
+
+
+audioname = None
+
 
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Update this with your desired origins
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
 
+
 @app.post("/upload-audio")
-async def upload_audio(audio: UploadFile = File(None)):
-    if audio:
-        unique_id = str(uuid.uuid4())
-        audio_file_name = f"static/audio_{unique_id}.mp3"
-        with open(audio_file_name, "wb") as f:
-            f.write(await audio.read())
-        return JSONResponse({"message": "Audio uploaded successfully", "unique_id": unique_id})
-    else:
-        return JSONResponse({"message": "No audio uploaded"})
+async def upload_audio(file: UploadFile = File(...)):
+    # Save the uploaded audio file
+    i = 1
+    while True:
+        if os.path.exists(f"audio{i}.mp3"):
+            i += 1
+        else:
+            break
+
+    with open(f"audio{i}.mp3", "wb") as f:
+        f.write(await file.read())
+
+    global audioname
+    audioname = f"audio{i}.mp3"
+    time.sleep(4)
+
+    return {"message": "Videos uploaded successfully"}
+
+
+@app.post("/upload-videos")
+async def upload_videos(files: List[UploadFile] = File(...)):
+    print(files)
+    i = 1
+    for file in files:
+        with open(f"video{i}.mp4", "wb") as f:
+            f.write(await file.read())
+        i+=1
+
+    return {"message": "Videos uploaded successfully"}
+
 
 @app.post("/upload")
-async def upload_file(files: List[UploadFile] = File(...), videoNumber: int = Form(...)):
-    unique_id = str(uuid.uuid4())
-    width = 426
-    height = 720
-    video_files = []
-    for file in files:
-        temp_file_name = f"static/temp_{unique_id}_{file.filename}"
-        with open(temp_file_name, "wb") as f:
-            f.write(await file.read())
+async def upload_file(file: UploadFile = File(...), videoNumber: int = Form(...)):
+    with open(f"video{videoNumber}.mp4", "wb") as f:
+        f.write(await file.read())
 
-        resized_file_name = f"static/resized_{unique_id}_{file.filename}"
-        print("File name: ", file.filename)
-        subprocess.run([
-            "ffmpeg", "-i", temp_file_name, "-vf",
-            f"scale={width}:{height}:force_original_aspect_ratio=increase,crop={width}:{height}",
-            resized_file_name
-        ])
-        video_files.append(resized_file_name)
-
-    # concatenate videos into one
-    list_file = f"static/list_{unique_id}.txt"
-    with open(list_file, "w") as f:
-        for video_file in video_files:
-            f.write(f"file '{os.path.join(os.getcwd(), video_file)}'\n")
-
-    # concatenate videos into one
-    final_clip_name = f"static/video_{unique_id}_{videoNumber}.mp4"
-    subprocess.run(["ffmpeg", "-f", "concat", "-safe", "0", "-i", list_file, "-c", "copy", final_clip_name])
-    os.remove(list_file)  # remove the list file
     # Generate thumbnail image for the uploaded video
-    video_capture = cv2.VideoCapture(final_clip_name)
+    video_capture = cv2.VideoCapture(f"video{videoNumber}.mp4")
     success, frame = video_capture.read()
     if success:
-        thumbnail_path = f"static/thumbnail_{unique_id}_{videoNumber}.jpg"
+        # Save the thumbnail as a temporary file
+        thumbnail_path = f"thumbnail{videoNumber}23.jpg"
         cv2.imwrite(thumbnail_path, frame)
+
+        # Read the thumbnail image and convert it to base64
+        with open(thumbnail_path, "rb") as thumbnail_file:
+            thumbnail_data = thumbnail_file.read()
+            thumbnail_base64 = base64.b64encode(thumbnail_data).decode("utf-8")
+
     else:
-        thumbnail_path = None
+        thumbnail_base64 = None
 
-    return JSONResponse({"message": "Videos uploaded successfully", "imagePath": thumbnail_path, "unique_id": unique_id})
+    return JSONResponse({"message": "Video uploaded successfully", "imagePath": thumbnail_base64})
 
-@app.get("/combine/{unique_ids}/{audio_id}")
-async def combine_videos(unique_ids: str, audio_id: str = None):
 
-    for file in os.listdir("static"):
-        if file.startswith("test_"):
-            os.remove(os.path.join("static", file))
 
-    unique_ids = unique_ids.split(",")
-    videos = [f"static/video_{unique_id}_{i+1}.mp4" for i, unique_id in enumerate(unique_ids)]
-    
-    width = 426
-    height = 720
-    unique_id = str(uuid.uuid4())
-    video_filters = []
-    ffmpeg_command = ["ffmpeg"]
+def generate_unique_filename():
+    timestamp = str(int(time.time()))
+    random_string = ''.join(random.choices(string.ascii_letters + string.digits, k=6))
+    unique_filename = timestamp + '_' + random_string
+    return unique_filename
 
-    for i in range(3):  # for 3 video sections
-        if i < len(videos):  # if video exists for this section
-            video = videos[i]
-            ffmpeg_command.extend(["-i", video])
-            video_filters.append(f"[{i}:v]scale={width}:{height},pad=426:720:(426-iw)/2:(720-ih)/2:black[v{i}]")  # pad video to required size
-            video_filters.append(f"[{i}:a]aformat=sample_fmts=fltp:sample_rates=44100:channel_layouts=stereo[a{i}]")
-        else:  # if no video exists for this section
-            video_filters.append(f"color=black:s=426x720:d=30[v{i}]")  # generate blank video
-            video_filters.append(f"aevalsrc=0:d=30[a{i}]")  # generate silent audio
 
-    if audio_id != "0":  # if audio_id is provided, merge the audio with the video
-        ffmpeg_command.extend(["-i", f"static/audio_{audio_id}.mp3"])
-        video_filters.append(f"[{len(videos)}:a]aformat=sample_fmts=fltp:sample_rates=22100:channel_layouts=stereo[a{len(videos)}]")
-        video_filters.append(f'{"[" + "][".join([f"a{i}" for i in range(len(videos) + 1)])}]amix=inputs={len(videos) + 1}[a]')
+
+@app.post("/combine")
+async def combine_videos(files: List[UploadFile] = File(...), audio: UploadFile = File(None)):
+
+
+
+    print(len(files))
+    file1 = generate_unique_filename()
+    audionames = generate_unique_filename()
+    outputname = generate_unique_filename()
+
+
+    i = 1
+    for i, file in enumerate(files, start=1):
+        print(file1)
+        with open(f"{file1}{i}.mp4", "wb") as f:
+            f.write(await file.read())
+        i+=1
+    count = 0
+
+    if audio is not None:
+        # Save the uploaded audio file
+        with open(f"{audionames}.mp3", "wb") as f:
+            f.write(await audio.read())
+            audios = f"-i {audionames}.mp3"
+            count = count + 1
+
     else:
-        video_filters.append(f'{"[" + "][".join([f"a{i}" for i in range(3)])}]amix=inputs={3}[a]')
-
-    video_filters.append(f'{"[" + "][".join([f"v{i}" for i in range(3)])}]hstack=inputs={3}[v]')
-
-    ffmpeg_command.extend(["-filter_complex", '; '.join(video_filters), "-map", "[v]", "-map", "[a]", "-b:v", "1000k", "-preset", "ultrafast", "-t", "30", "-r", "24", f"static/test_{unique_id}.mp4"])
-    subprocess.run(ffmpeg_command)
-
-    # remove all the temporary files
-    for video in videos:
-        os.remove(video)
-    
-    for file in os.listdir("static"):
-        if file.startswith("video_") or file.startswith("resized_") or file.startswith("temp_") or file.startswith("thumbnail_") or file.startswith("audio_"):
-            os.remove(os.path.join("static", file))
-    
-    return FileResponse(f"static/test_{unique_id}.mp4", media_type="video/mp4")
+        audios = ""
+        print("no audio")
 
 
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="localhost", port=8000)
+    global audioname
+
+    length = 6
+
+
+    audio_merge = ";"
+    command = ['ffprobe', '-v', 'error', '-select_streams', 'a:0', '-show_entries', 'stream=codec_type', '-of', 'default=noprint_wrappers=1:nokey=1', "video1.mp4"]
+    result = subprocess.run(command, capture_output=True, text=True)
+    if result.returncode == 0 and result.stdout.strip() == 'audio':
+        audio_merge = audio_merge + "[0:a]"
+        count = count + 1
+    else:
+        print("no audio")
+        pass
+
+    command = ['ffprobe', '-v', 'error', '-select_streams', 'a:0', '-show_entries', 'stream=codec_type', '-of', 'default=noprint_wrappers=1:nokey=1', "video2.mp4"]
+    result = subprocess.run(command, capture_output=True, text=True)
+    if result.returncode == 0 and result.stdout.strip() == 'audio':
+        audio_merge = audio_merge + "[1:a]"
+        count = count + 1
+    else:
+        pass
+
+    command = ['ffprobe', '-v', 'error', '-select_streams', 'a:0', '-show_entries', 'stream=codec_type', '-of', 'default=noprint_wrappers=1:nokey=1', "video3.mp4"]
+    result = subprocess.run(command, capture_output=True, text=True)
+    if result.returncode == 0 and result.stdout.strip() == 'audio':
+        audio_merge = audio_merge + "[2:a]"
+        count = count + 1
+    else:
+        pass
+
+
+    if count > 0:
+        audio_merge = audio_merge + f"amerge=inputs={count}[a]"
+        maping = "-map \"[a]\""
+
+    else:
+        audio_merge = ""
+        maping = ""
+        
+    print(audio_merge)
+
+    # run a single command
+    command = f"""ffmpeg -y -i {file1}1.mp4 -i {file1}2.mp4 -i {file1}3.mp4 {audios} -vsync 2 -filter_complex "[0:v]scale=426:720[v0];[1:v]scale=426:720[v1];[2:v]scale=426:720[v2];[v0][v1][v2]hstack=3,scale=1280:720[v]{audio_merge} " -map "[v]" {maping} -c:v libx264 -crf 23 -preset veryfast -c:a libmp3lame -b:a 128k -t {length} {outputname}.mp4"""
+    subprocess.run(command, shell=True)
+
+    return FileResponse(f"{outputname}.mp4", media_type="video/mp4")
